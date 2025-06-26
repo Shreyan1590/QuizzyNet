e78d3fc22118a8a185316ad4fbf7e05305c40c0e
Shreyan1590 <shreyanofficial25@gmail.com>
2025-06-26 10:22:00 +0530

    Same as before

--- quizzynet/vite.config.ts
+++ quizzynet/vite.config.ts
@@ -1,10 +1,28 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
+  build: {
+    rollupOptions: {
+      external: [
+        '@mui/x-date-pickers',
+        '@mui/x-date-pickers/DateTimePicker',
+        '@mui/x-date-pickers/LocalizationProvider',
+        '@mui/x-date-pickers/AdapterDateFns',
+        '@mui/material',
+        '@emotion/react',
+        '@emotion/styled'
+      ]
+    }
   },
-});
+  optimizeDeps: {
+    include: [
+      '@mui/x-date-pickers',
+      '@mui/material',
+      '@emotion/react',
+      '@emotion/styled',
+      'date-fns'
+    ]
+  }
+})
